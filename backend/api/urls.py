e533b9c65906c4014259668e6575751e53fa0a1f
maru-koch <nwokochafranklyn@gmail.com>
2022-06-19 00:46:32 +0100

    --no-edit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,7 @@ from .views import (
     EnergyAnalyticView,
     RequestDevicesView,
     RetrieveDeviceView,
-    EnergyConsumptionView, 
-    get_energy,
-    get_metrics,
+    EnergyConsumptionView
 )
 
 urlpatterns = [
@@ -15,9 +13,7 @@ urlpatterns = [
     path('device/<pk>/', RetrieveDeviceView.as_view(), name="device"),
     path('device/request/<pk>', RequestDevicesView.as_view(), name="request"),
     path('device/energy/', EnergyConsumptionView.as_view(), name="energy"), 
-    path('device/energy/all/', get_energy, name="energy"),  # . 1
     path('device/analytics/all', EnergyAnalyticView.as_view(), name="analytics"),
-    path('device/metrics/all/', get_metrics, name="metrics")  # average, minimum, and maximum
 ]
 
 # http://127.0.0.1:8000/api/device/analytics/
