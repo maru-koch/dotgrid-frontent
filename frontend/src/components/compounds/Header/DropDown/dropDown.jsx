4c5c8f631a7f1b4a25229706e9cbbebc77041253
maru-koch <nwokochafranklyn@gmail.com>
2022-07-12 11:33:15 +0100

    feature - dropdown menu
    begin styling the component

--- frontend/src/components/compounds/Header/DropDown/dropDown.jsx
+++ frontend/src/components/compounds/Header/DropDown/dropDown.jsx
@@ -1,12 +1,12 @@
 import classes from './dropDowm.module.css'
 const DropDownItem=({icon, title, description})=>{
     return (
-        <main className={classes.dropdown__container}>
-            <div className={classes.dropdown__container}>
-                <section className={classes.dropdown__section_left}>
+        <main className={classes.dropdown_item__container}>
+            <div className={classes.dropdown_item__container}>
+                <section className={classes.dropdown_item__section_left}>
                     <i className={icon}></i>
                 </section>
-                <section className={classes.dropdown__section_right}>
+                <section className={classes.dropdown_item__section_right}>
                     <h2>{title}</h2>
                     <p>{description}</p>
                 </section>
@@ -17,14 +17,16 @@ const DropDownItem=({icon, title, description})=>{
 export const  DropDown=(items)=>{
     // Go through the items, and output a drop down item for each
     return (
-        <main>
-            {items.map((item,idx)=>
-            <DropDownItem 
-                key ={idx } 
-                icon = {item.icon}
-                title = {item.title}
-                description = {item.description}
-            />)}  
+        <main className={classes.dropdown__container}>
+            <section className={classes.dropdown_item__section}>
+                {items.map((item,idx)=>
+                    <DropDownItem 
+                        key ={idx } 
+                        icon = {item.icon}
+                        title = {item.title}
+                        description = {item.description}
+                    />)}  
+            </section>
         </main>
     )
 }
