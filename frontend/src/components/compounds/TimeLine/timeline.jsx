fab595fcfb1b424dced10076fcbe7b1c6500adca
maru-koch <nwokochafranklyn@gmail.com>
2022-07-16 15:38:18 +0100

    feature - timeline

--- frontend/src/components/compounds/TimeLine/timeline.jsx
+++ frontend/src/components/compounds/TimeLine/timeline.jsx
@@ -5,9 +5,7 @@ export const TimeLine=()=>{
     return(
         <div className="timeline__container">
             <div className="timelineItem__wrapper">
-                {timeLineData.map((data, idx)=>
-                    (<TimeLineItem data ={data} key={idx} id={idx+1}/>)
-                )}
+                {timeLineData.map((data, idx)=> (<TimeLineItem data ={data} key={idx} id={idx+1}/>))}
             </div>
         </div>
     )
