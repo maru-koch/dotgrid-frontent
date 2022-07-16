2c8d3b94cb3f212a7f3393eb425a7227c5cd30f4
maru-koch <nwokochafranklyn@gmail.com>
2022-07-16 15:30:20 +0100

    --no-edit

--- frontend/src/components/compounds/TimeLine/timeline.jsx
+++ frontend/src/components/compounds/TimeLine/timeline.jsx
@@ -1,6 +1,14 @@
-import {timelineItem} from './timelineItem';
+import {TimeLineItem} from './timelineItem';
 import {timeLineData} from '../../../constants/timelineData'
 
-export TimeLine=()=>{
-
+export const TimeLine=()=>{
+    return(
+        <div className="timeline__container">
+            <div className="timelineItem__wrapper">
+                {timeLineData.map((data, idx)=>
+                    (<TimeLineItem data ={data} key={idx} id={idx+1}/>)
+                )}
+            </div>
+        </div>
+    )
 }
