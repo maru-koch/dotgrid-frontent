3efc001e0917ddfb516581f0c1502c08bfeccb8a
maru-koch <nwokochafranklyn@gmail.com>
2022-07-17 12:45:02 +0100

    feature - section image

--- frontend/src/components/elements/Sections/SectionImage/sectionImage.jsx
+++ frontend/src/components/elements/Sections/SectionImage/sectionImage.jsx
@@ -1,17 +1,22 @@
+import PropTypes from 'prop-types'
 
-import starter_image from '../../../assets/images/solar_starter_pack.png';
 import classes from './starterImage.module.css';
 import {Button} from '../../elements'
 
-export const SectionImage=()=>{
+export const SectionImage=({btn, image})=>{
     return(
         <div className ={classes.starter__container}>
             <div className ={classes.starter__wrapper}>
-                <img src={starter_image} alt="Starter pack"/>
+                <img src={image} alt="Starter pack"/>
                 <div className ={classes.starter__btn_wrapper}>
-                     <Button text ="Get Solar Pack" />
+                     {btn && <Button text ="Get Solar Pack" />}
                 </div>
             </div>
         </div>
     )
+}
+
+SectionImage.propTypes = {
+    btn: PropTypes.bool.isRequired,
+    image: PropTypes.string
 }
