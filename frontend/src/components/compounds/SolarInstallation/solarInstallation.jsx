26e8823ac41d43efb3a390f0b8f1f4562816d40f
maru-koch <nwokochafranklyn@gmail.com>
2022-07-17 12:25:59 +0100

    feature - solar installation
    added the image, section text and solar project sections

--- frontend/src/components/compounds/SolarInstallation/solarInstallation.jsx
+++ frontend/src/components/compounds/SolarInstallation/solarInstallation.jsx
@@ -1,10 +1,21 @@
-import classes from './getLoan.module.css'
+import { SolarProjects } from '../../molecules'
+import { SectionText, Section, SectionWrapper } from '../../compounds'
+import solar_panel from '../../../assets/images/solar_panel.png'
+
+const word ={
+    title:'Section Title',
+    description: '',
+    button_text: 'Order Solar PV Panel'
+}
+
 export const SolarInstallation=()=>{
     return(
         <Section>
             <SectionWrapper>
-                <
+                <Image image={solar_panel}/>
+                <SectionText title ={word.title} description ={word.desc}/>
             </SectionWrapper>
+            <SolarProjects/>
         </Section>
     )
 }
