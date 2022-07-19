b9e19fcfff3781a311dac972ece2c7b5ddf45495
maru-koch <nwokochafranklyn@gmail.com>
2022-07-19 13:52:11 +0100

    --no-edit

--- frontend/src/components/elements/Sections/index.js
+++ frontend/src/components/elements/Sections/index.js
@@ -1,8 +1,8 @@
 export * from './Section';
-export * from './SectionText';
 export * from './SectionContainer';
 export * from './SectionWrapper';
 export * from './SectionHeader';
 export * from './SectionImage';
 export * from './Card';
+export * from './SectionText'
 
