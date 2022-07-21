be913f6cd2ada0f6bb509c93bbcb7f47066d2b42
maru-koch <nwokochafranklyn@gmail.com>
2022-07-21 19:35:32 +0100

    added how it works data

---
+++ frontend/src/constants/howItWorksData.js
@@ -0,0 +1,22 @@
+export const timeLineData =[
+    {
+        icon: 'fa fa-request',
+        description: "Request a demo",
+        url: 'http://localhost:3000/solar-installation'
+    },
+    {
+        icon: 'fa fa-request',
+        description: "Get Your Custom Solution",
+        url: ''
+    },
+    {
+        icon: 'fa fa-request',
+        description: "System Configuration and Automation",
+        url: ''
+    },
+    {
+        icon: 'fa fa-user-headset',
+        description: "After-sales Support",
+    }
+      
+]
