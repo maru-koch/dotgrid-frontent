5681b85cd6ede63993a11675f1a1eed96d5e3d9b
maru-koch <nwokochafranklyn@gmail.com>
2022-07-21 19:38:10 +0100

    --no-edit

--- frontend/src/components/compounds/HowItWorks/HowCard/cardItem.jsx
+++ frontend/src/components/compounds/HowItWorks/HowCard/cardItem.jsx
@@ -7,9 +7,9 @@ export const CardItem = ({icon, desc }) =>{
     const navigate = useNavigate()
     
     return (
-        <Card style ={classes.card} onClick={()=>navigate(url)}>
+        <Card style ={classes.card}>
             <div>
-                <div>
+                <div className ={classes.circle}>
                      <i className={icon}></i>
                 </div>
             </div>
