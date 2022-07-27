3cbae2991482355d71c5c4e1506583263f43ff3f
maru-koch <nwokochafranklyn@gmail.com>
2022-07-27 22:20:14 +0100

    SOLAR ANALYTICS
    concluded the why analytics section

--- frontend/src/components/compounds/WhyAnalytics/whyAnalytics.jsx
+++ frontend/src/components/compounds/WhyAnalytics/whyAnalytics.jsx
@@ -1,41 +1,44 @@
 
 import { Section, SectionText, SectionWrapper, SectionImage, SectionHeader} from '../../elements'
-import solar_panel from '../../../assets/images/dotgrid_solar_panel.png'
-import solar_pack from '../../../assets/images/solar_starter_pack.png'
+import analytics_phone from '../../../assets/images/analytics_phone.png'
+import analytics_computer from '../../../assets/images/analytics_computer.png'
 
 const word =[{
     title:'Energy Automation',
     desc: `
-                Automate your energy consumption by setting when you want your devices to turn on or off. You can select the time and how long you want a particular appliance to be on`,
+                Automate your energy consumption by setting when you want 
+                your devices to turn on or off. You can remotely select 
+                the time and how long you want a particular appliance to 
+                be on to save Energy and optimize performance`,
 
     button_text: 'Order Solar PV Panel'
 },{
     title:'Derive Insight',
     desc: `
                Monitor your energy consumption pattern by looking at 
-               the visualization of your energy data. Our AI driven analytics when
+               the visualization of your energy data. Our AI driven analytics will
                guide you in making the best decision.`,
 
     button_text: 'Request Demo'
 }
 ]
 
-export const whyAnalytics =()=>{
+export const WhyAnalytics =()=>{
     return(
         <Section>
             <SectionHeader title = "Why Dotgrid Analytics?" subtitle = ""/>
             <SectionWrapper>
                 <SectionImage 
-                    image={solar_pack} 
+                    image={analytics_computer} 
                     height={'100%'} 
-                    width={'50%'}
-                    wrapperStyle={{height:500, width: 500}}
+                    width={'100%'}
+                    wrapperStyle={{height:600, width: 500}}
                     containerStyle={{display:'flex', justifyContent: 'center', alignItems: 'center'}}/>
                 <SectionText 
                     title ={word[0].title} 
                     description ={word[0].desc}
                     btn={true}
-                    btnText="Order Solar PV"/>
+                    btnText="Request Demo"/>
             </SectionWrapper>
          <SectionWrapper bg ={{backgroundColor:"#e6f3f8"}}>
 
@@ -46,7 +49,7 @@ export const whyAnalytics =()=>{
                 btnText="Request Demo"/>
 
             <SectionImage 
-                image={solar_panel} 
+                image={analytics_phone} 
                 height={'100%'} 
                 width={'50%'}
                 wrapperStyle={{height:700, width: 500}}
