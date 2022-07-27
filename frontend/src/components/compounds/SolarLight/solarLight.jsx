b3cf41b4dcb7556ebcb1fde15d2d55cfe1c665db
maru-koch <nwokochafranklyn@gmail.com>
2022-07-27 14:00:09 +0100

    added solar light image

--- frontend/src/components/compounds/SolarLight/solarLight.jsx
+++ frontend/src/components/compounds/SolarLight/solarLight.jsx
@@ -1,14 +1,13 @@
 
 import { SectionWrapper, SectionImage, SectionHeader } from '../../elements'
-import solar_panel from '../../../assets/images/dotgrid_solar_panel.png'
-
+import solar_bulb from '../../../assets/images/solar_bulb.jpeg'
 
 
 export const SolarLight=()=>{
     return(
         <SectionWrapper>
             <SectionHeader title = "Uninterrupted Power Supply" subtitle="Enjoy 24/7 Clean energy without environmental polutions"/>
-            <SectionImage image={solar_panel}/>
+            <SectionImage image={solar_bulb}/>
         </SectionWrapper>
     )
 }
