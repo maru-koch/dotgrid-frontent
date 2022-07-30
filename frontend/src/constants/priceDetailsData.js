b2b5130cd47e118628a9519dafeef7dedcd5096d
maru-koch <nwokochafranklyn@gmail.com>
2022-07-30 01:28:43 +0100

    feature - select model
    modified the structure of the price model data

--- frontend/src/constants/priceDetailsData.js
+++ frontend/src/constants/priceDetailsData.js
@@ -1,35 +1,48 @@
 
-export const priceDetailsData = [
-    {
-        basic:{
+export const priceDetailsData = {
+    individual:{
+        plans:[
+        {   
+            priceModel:'basic',
             price: 'free',
             features: ['View Energy generated', '1 control device']
         },
-
-        Starter:{
-            price: '$ 20 / year',
+        {   priceModel:'starter',
+            price:  '$ 20 / year',
             features: ['View Energy generated', 'automate energy consumptiion', '2 control devices']
         },
 
-        Pro:{
+        {
+            priceModel:'pro',
             price: '$ 100 / year',
             features: ['View Energy generated', 'Monitor energy consumption', 'upto 5 control devices', 'Energy sharing', 'Energy optimization', 'Control appliances remotely']
-        }
+        }],
+
+        description: "Suitable for an Appartment or home use. Give you the autonomy to control your energy consumption and save cost"
     },
-    {
-        basic:{
+
+    enterprice:{
+        plans:[
+        {   
+            priceModel:'basic',
             price: '$ 200 / year ',
             features: ['Two Users', 'View Energy generated', 'connect upto 5 devices', 'Reporting', 'Export Data as CSV']
         },
-
-        Starter:{
+        {   priceModel:'starter',
             price: '$ 500 / year',
             features: ['max of 10 users', 'connect 20 control devices', 'View Energy generated', 'Reporting', 'Export Data as CSV']
         },
 
-        Pro:{
+        {
+            priceModel:'pro',
             price: '$ 1000 / year',
             features: ['unlimited Users', 'Manage Clients Data', 'Reporting features', 'Export Data as CSV', 'Grid monitoring and Optimization']
-        }
-    }]
+        }],
+
+        description: "Suitable for cities or Estates or mini-grid management. helps managers derive energy insight and make the best decisions base on patterns in energy generation and consumption"
+    },
+}
+    
+   
+   
 
