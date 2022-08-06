f82f017ce6aadce853ce83f7cc8ff5e79ce8d50c
maru-koch <nwokochafranklyn@gmail.com>
2022-08-06 10:07:48 +0100

    added the learn more page

---
+++ frontend/src/components/compounds/LearnMore/learnMore.jsx
@@ -0,0 +1,12 @@
+import {Section, SectionImage, SectionWrapper, SectionText} from '../../elements'
+
+export const LearnMore =({details})=>{
+    return (
+        <Section>
+            <SectionImage/>
+            <SectionText/>
+            <SectionWrapper/>
+            <SectionWrapper/>
+        </Section>
+    )
+}
---
+++ frontend/src/pages/LearnMore/learnMorePage.jsx
@@ -0,0 +1,10 @@
+import {PageLayout} from '../../layout'
+import {LearnMore} from '../../components/compounds''
+
+export const LearnMorePage=()=>{
+    return (
+        <PageLayout>
+            <LearnMore/>
+        </PageLayout>
+    )
+}
