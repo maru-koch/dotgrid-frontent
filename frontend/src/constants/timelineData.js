dd60b5fd741c9ee41a3f07a34b5e0d9d22945f6a
maru-koch <nwokochafranklyn@gmail.com>
2022-08-09 17:29:05 +0100

    wrapped up the styling of the testimonies components

--- frontend/src/constants/timelineData.js
+++ frontend/src/constants/timelineData.js
@@ -2,18 +2,19 @@ export const timeLineData =[
     // Data for the timeline component in the get-started page
     {
         title: 'Solar Installation',
-        description: `Order and get your Solar panels installed by our energy experts. Do not have money? Do not worry. You can get your complete solar PV compact fully funded through our Energy Loan Scheme`,
+        description: `Order and get your Solar panels installed by our energy experts. `,
         url: 'http://localhost:3000/solar-installation'
     },
     {
         title: 'Smart Metering',
-        description: "Remotely monitor the amount of Energy you are generating and how it is being consumed. Use our IoT Solutions to remotely automate the energy consumption of the smart appliances in your home, office or factory from anywhere in the world",
+        description: "Remotely monitor the amount of Energy you are generating and how ",
         url: ''
     },
     {
-        title: 'Energy Analytics',
-        description: "Track your pattern of Energy consumption and save money by optimizing the rate your appliance consume energy. You can also view The total energy you have consumed over a period of time, and forcast future energy generation",
+        title: 'Smart Metering',
+        description: "Remotely monitor the amount of Energy you are generating and how ",
         url: ''
     },
+    
       
 ]
