be7c5e6f1915a0aad709aeba9db99ace4c3ef727
maru-koch <nwokochafranklyn@gmail.com>
2022-08-10 12:26:01 +0100

    installed react tab component

--- frontend/src/components/compounds/SolarPVCalculator/solarPVCalculator.jsx
+++ frontend/src/components/compounds/SolarPVCalculator/solarPVCalculator.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Section, SectionContainer, SectionWrapper, Button } from '../../elements';
 import { EnergyConsumed } from "./EnergyConsumption";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 export const SolarPVCalculator =()=>{
@@ -8,11 +10,18 @@ export const SolarPVCalculator =()=>{
 
 
     return (
-        <Section>
-            <SelectEnergy setEnergy={setEnergy}/>
-            <SectionContainer>
-                {energy = "consumed"? <EnergyConsumed/>:""}
-            </SectionContainer>
-        </Section>
+        <Tabs defaultIndex={1} selectedIndex={energy} onSelect={(index) => setEnergy(index)}>
+            {/* <SelectEnergy setEnergy={setEnergy}/> */}
+            <TabList>
+                <Tab>Energy Consumed</Tab>
+                <Tab>Energy Consumed</Tab>
+            </TabList>
+            <TabPanel>
+                <h1>consumed</h1>
+            </TabPanel>
+            <TabPanel>
+                <h1>generated</h1>
+            </TabPanel>
+        </Tabs>
     )
 }
