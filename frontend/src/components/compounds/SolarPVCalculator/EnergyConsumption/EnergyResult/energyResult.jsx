0941e2ea8635213f6e724e8fdeabdbefac69e4c7
maru-koch <nwokochafranklyn@gmail.com>
2022-09-11 19:14:18 +0100

    added the number of appliances in the EnergyResult component

--- frontend/src/components/compounds/SolarPVCalculator/EnergyConsumption/EnergyResult/energyResult.jsx
+++ frontend/src/components/compounds/SolarPVCalculator/EnergyConsumption/EnergyResult/energyResult.jsx
@@ -1,15 +1,28 @@
-
+import PropTypes from 'prop-types'
 import classes from "./energyResult.module.css"
 
-export const EnergyResult=({total})=>{
-    // Displays the total energy consumed by all appliances
+export const EnergyResult=({total, num})=>{
+    /*
+        This components displays the total energy consumed by all appliances components
+        and the total number of appliances
+            -------------
+            parameters: 
+                total: total energy consumed by all appliances
+                num: total number of appliances
+     */
     return (
         <div className={classes.tableFooter}>
             <tr>
+                <td>{num? `${num} Appliances`: null}</td>
                 <td className={classes.tableTotal__Title} >Total Energy Consumed 'watt/per day'</td>
                 <td className={classes.tableTotal} >{total}</td>
             </tr>
         </div>
                 
     )
+}
+
+EnergyResult.propTypes = {
+    total: PropTypes.number,
+    num: PropTypes.number
 }
